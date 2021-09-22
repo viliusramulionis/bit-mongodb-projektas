4ba7a214d07074a3dfb135355b2a86c9b9e6fcc9
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2021-09-22 21:24:26 +0300

    Atnaujinti routeriai

--- controllers/irasai.js
+++ controllers/irasai.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+
 const irasaimodel = mongoose.model('irasai');
 
 router.get('/', (req, res) => {
-
-    //Irasu pridejimas
-
-    // var irasas = new irasaimodel();
-    // irasas.pavadinimas = 'Testawdawd';
-    // irasas.turinys = 'JAWdawdawdawd';
-    // irasas.data = '2021-09-20';
-    // irasas.save();
-
     //Irasu paemimas
 
     irasaimodel.find( (erroras, informacija) => {
@@ -26,6 +18,7 @@ router.get('/', (req, res) => {
             informacija.forEach(function(item) {
                 var data = new Date(item.data);
                 item.data = data.toLocaleDateString('lt-LT');
+                item._id = item._id.toString();
             });
 
             res.render('list', {data: informacija});
@@ -33,9 +26,85 @@ router.get('/', (req, res) => {
         } else {
             res.send('Ivyko klaida');
         }
-    }).lean();
+    })
+    //Pakeiciame modelio kalbos atpazinima i lietuviska
+    .collation({locale: "lt" })
+    //isrusiuojame gauta informacija zemejimo tvarka (jeigu norime atvirksciai rasome -1)
+    .sort({pavadinimas : 1})
+    //grazina apdorota informacijos paketa 
+    //https://mongoosejs.com/docs/tutorials/lean.html
+    .lean();
 
     
+});
+
+router.get('/pridejimas', (req, res) => {
+    res.render('add');
+});
+
+router.post('/edit_submit', (req, res) => {
+
+    irasaimodel.updateOne({_id: req.body.id}, {
+        pavadinimas: req.body.pavadinimas,
+        turinys: req.body.turinys,
+        data: req.body.data
+    });
+
+    res.redirect('/irasai');
+});
+
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+
+    irasaimodel.findById(id).lean()
+    .then(info => {
+
+        var data = new Date(info.data);
+        info.data = data.toLocaleDateString('lt-LT');
+        
+        res.render('edit', { edit : info });
+    })
+    .catch(err => {
+        res.json({
+            response: 'fail',
+            message: err.message
+        });
+    });
+
+});
+
+router.post('/submit', (req, res) => {
+
+    //Irasu pridejimas
+
+    var irasas = new irasaimodel();
+    irasas.pavadinimas = req.body.pavadinimas;
+    irasas.turinys = req.body.turinys;
+    irasas.data = req.body.data;
+    irasas.save();
+
+    // console.log(req.body);
+    res.redirect('/irasai');
+});
+
+router.get('/paieska/:s', (req, res) => {
+    const s = req.params.s;
+
+    irasaimodel.find( {$pavadinimas: { $regex: s}}, (erroras, informacija) => {
+        if(!erroras) {
+            res.json(informacija);
+        } else {
+            console.log(erroras);
+            res.send('Ivyko klaida');
+        }
+    })
+    //Pakeiciame modelio kalbos atpazinima i lietuviska
+    .collation({locale: "lt" })
+    //isrusiuojame gauta informacija zemejimo tvarka (jeigu norime atvirksciai rasome -1)
+    .sort({pavadinimas : 1})
+    //grazina apdorota informacijos paketa 
+    //https://mongoosejs.com/docs/tutorials/lean.html
+    .lean();
 });
 
 module.exports = router;
